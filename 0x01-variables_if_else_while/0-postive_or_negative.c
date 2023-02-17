7f9314677434de68ef97bcdd73051f836e1796a8
Nebiyu <nebamami@gmail.com>
2023-02-17 08:28:10 -0800

    c variables

---
+++ 0x01-variables_if_else_while/0-postive_or_negative.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - A program tp print whether the number stored in the variable n is postive or negative
+ * n - the variable will store a different vallue
+ * 
+ * Return: 0(seccess)
+ */
+int main(void)
+{
+	int n;
+	if(n>o)
+	{
+		printf("%d is a positive\n",n);
+	}
+	else if(n==0)
+	{
+		printf("%d is zero\n",n);
+	}
+	else
+	{
+		printf("%d is negative\n",n);
+	}
+	return(0);
+}

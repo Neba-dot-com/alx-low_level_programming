c8203c6a3bc3a774864d5346bb4b0080cda28a99
Nebiyu <nebamami@gmail.com>
2023-02-17 00:30:26 -0800

    standard error

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *  main - A program that prints a a piece of text using fprintf
+ *
+ *  Return: 0(Success)
+ */
+int main(void)
+{
+fprintf(stderr,"%s","and that piece of art is useful\" -Dora korpar, 2015-10-19");
+return (0);
+}

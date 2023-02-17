9bedf243ed0e610191800c9925b50516ee534bc4
Nebiyu <nebamami@gmail.com>
2023-02-17 15:02:03 -0800

    print hexadecimal alphabet

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - A program prints all the nubers of base 16 i lowercase
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num <= 15)
+	{
+		puchar(num + '%x');
+
+		num = num + 1;
+	}
+	putchar('\n');
+	return (0);
+}

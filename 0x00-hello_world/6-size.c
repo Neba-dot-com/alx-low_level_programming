d0c704140a31e29ba61f0caa5f0ec87df4d75c3d
Nebiyu <nebamami@gmail.com>
2023-02-16 18:07:11 -0800

    million trials

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -11,10 +11,10 @@ int main(void)
 	long int c;
 	long long int d;
 	float f;
-printf("Size of a char: %zu bytes\n", sizeof(a));
-printf("Size of an int: %zu bytes\n", sizeof(b));
-printf("Size of a long int: %zu bytes\n", sizeof(c));
-printf("size of a long long int: %zu bytes\n", sizeof(d));
-printf("Size of a float: %zu bytes\n", sizeof(f));
+printf("Size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+printf("Size of an int: %lu byte(s)\n", (unsigned long)sizeof(b));
+printf("Size of a long int: %lu bytes(s)\n", (unsigned long)sizeof(c));
+printf("size of a long long int: %lu bytes(s)\n", (unsigned long)sizeof(d));
+printf("Size of a float: %lu bytes(s)\n", (unsigned long)sizeof(f));
 return (0);
 }

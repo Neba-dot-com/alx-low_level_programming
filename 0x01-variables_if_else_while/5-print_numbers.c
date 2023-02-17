045af6daa0a3cf48257a12ae49d3d5ec81d2a328
Nebiyu <nebamami@gmail.com>
2023-02-17 14:09:04 -0800

    print num

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - A program that prints digit
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num < 10)
+	{
+		printf("%d", n);
+
+		num = num + 1;
+	}
+	return (0);
+}

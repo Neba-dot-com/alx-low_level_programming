d65d2682bb29489df52d789526cdf4578a36cd5e
Nebiyu <nebamami@gmail.com>
2023-02-17 13:57:49 -0800

    print lowercase

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ *main - A program that print a lowercase
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	char a;
+
+	a = 'a';
+
+	while (a <= 'z')
+	{
+		if (a != q && a != e)
+		{
+			putchar(a);
+		}
+		a = a + 1;
+	}
+	return (0);
+}

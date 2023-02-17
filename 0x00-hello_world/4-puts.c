f3944f1b4393be52ef00548608e6a5e25493e997
Nebiyu <nebamami@gmail.com>
2023-02-16 16:22:12 -0800

    print in puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,2 +1,9 @@
-#!/bin/bash
-	puts("Programing is like building a multilingual puzzle");
+#include <stdio.h>
+/**
+ * main Aprogrm that prints a line with puts function
+ * Returen:0 (success)
+ * /
+ int main(void)
+ {
+ pts("Programing is like building a multilingual puzzle");
+ }

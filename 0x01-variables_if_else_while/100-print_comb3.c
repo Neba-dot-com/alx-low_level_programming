f2d89a7c359f62f386adbede8bcdba088821d83d
Nebiyu <nebamami@gmail.com>
2023-02-17 15:59:14 -0800

    print comb3

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+/**
+ * main - intry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	for (int i = 0; i <= i; i++)
+	{
+		for (int j = 0; j <= 9; j++)
+		{
+			if (i < j)
+			{
+				putchar(i + '0');
+				putchar(j + '0');
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}

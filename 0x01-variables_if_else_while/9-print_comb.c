d87fd5f69971b6639baf08d353572ae4f65f1404
Nebiyu <nebamami@gmail.com>
2023-02-17 15:31:40 -0800

    print comb

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,29 @@
+# include <stdio.h>
+/**
+ * main - intry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int num;
+
+	num = 0;
+
+	while (num <= 9)
+	{
+		putchar(num + '0');
+		if (num <= 8)
+		{
+			putchar(',');
+		}
+		else
+		{
+			putchar('$');
+		}
+		putchar(' ');
+		num = num + 1;
+	}
+	putchar('\n');
+	return (0);
+}

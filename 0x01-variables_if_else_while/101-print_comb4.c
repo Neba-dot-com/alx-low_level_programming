2ebbdb069a6c9177dcf2268c6c0637bd24fe9cd0
Nebiyu <nebamami@gmail.com>
2023-02-18 13:41:58 -0800

    print comb4

---
+++ 0x01-variables_if_else_while/101-print_comb4.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - entry point
+ *
+ * Return: 0(success)
+ */
+int main(void)
+{
+	int i;
+	int j;
+	int k;
+
+	i = 0;
+
+	while (i <= 7)
+	{
+		j = 1;
+		while (j <= 8)
+		{
+			k = 2;
+			while (k <= 9)
+			{
+				putchar(i + '0');
+				putchar(j + '0);
+				putchar(k + '0');
+				if ((i == 7) && (j == 8) && (k == 9))
+				{
+					putchar('\n');
+				}
+				else
+				{
+					putchar(', ');
+					putchar(' ');
+				}
+				k++;
+			}
+
+			j++;
+		}
+
+		i++;
+	}
+
+	return (0);
+}
+

275b65a81b3993b3d27f7d970d3671781eba52be
Nebiyu <nebamami@gmail.com>
2023-02-22 01:19:17 -0800

    check sign

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,17 +11,17 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar('1' + '0');
+		_putchar('1');
 	}
 	else if (n == 0)
 	{
 		_putchar('0');
-		_putchar('0' + '0');
+		_putchar('0');
 	}
 	else
 	{
 		_putchar('-');
-		_putchar('1' + '0');
+		_putchar('1');
 	}
 	return (0);
 }

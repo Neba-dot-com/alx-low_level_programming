abc40dd79c4bdb1a8c0a0f6478010322aa78d6c7
Nebiyu <nebamami@gmail.com>
2023-02-23 12:55:17 -0800

    print uppercase

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+
+/**
+ *_isupper - a program that checkes for uppercase character
+ *@c: a parameter as input
+ *Return: int
+ */
+
+int _isupper(int c)
+{
+	if (c >= 'A' && C <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+}

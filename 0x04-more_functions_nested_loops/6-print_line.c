007a55bb89bb50019e076197e5334cd237d2ec23
Nebiyu <nebamami@gmail.com>
2023-02-23 23:34:15 -0800

    more num

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * print_line - draws a strait line
+ * @n: input
+ * Return: void
+ */
+void print_line(int n)
+{
+	int i;
+
+	if (n > 0)
+	{
+		i = 0;
+		while (i < c)
+		{
+			_putchar('_');
+			j++;
+		}
+	}
+	_putchar('\n');
+}

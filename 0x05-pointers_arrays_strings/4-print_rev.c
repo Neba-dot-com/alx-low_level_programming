5da476055da000efe875a23117c900edb9af5eb0
Nebiyu <nebamami@gmail.com>
2023-02-28 05:46:54 -0800

    rev

--- 0x05-pointers_arrays_strings/4-print_rev.c
+++ 0x05-pointers_arrays_strings/4-print_rev.c
@@ -3,34 +3,24 @@
  *print_rev- a  function that print string in reverse
  * @s:a parameter
  * Return: void
- * _strlen - a function counter that count the length of string
  */
-int _strlen(char *s);
-void print_rev(char *s);
-int _strlen(char *s)
-{
-	int counter;
-
-	counter = 0;
-	while (*s != '\0')
-	{
-		counter++;
-		s++;
-	}
-	return (counter);
-}
 void print_rev(char *s)
 {
+	char rev;
 	int counter;
-	char *p;
 	int i;
 
-	counter = _strlen(s);
-	*p = &s[counter - 1];
+	rev = s[0];
+	counter = 0;
 
-	for (i = 0; i < counter; i++)
-	{
-		_putchar(*p++);
-	}
-	_putchar('\n');
+	while (s[counter] != '\0')
+	counter++;
+
+		for (i = 0; i < counter; i++)
+		{
+			counter--;
+			rev = s[i];
+			s[i] = s[counter];
+			s[counter] = rev;
+		}
 }

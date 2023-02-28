34984e01ae855588731743696fe77dbdde435f8f
Nebiyu <nebamami@gmail.com>
2023-02-28 08:01:51 -0800

    print even

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -10,16 +10,16 @@ void puts2(char *str)
 	int c = 0;
 	int i;
 
-	while (s[c] != '\0')
+	while (str[c] != '\0')
 	{
 	c++;
 	}
 
 	for (i = 0; i < c; i++)
 	{
-		if (s[i] % 2 == 0)
+		if (str[i] % 2 == 0)
 		{
-			_putchar(s[i]);
+			_putchar(str[i]);
 		}
 	}
 	_putchar('\n');

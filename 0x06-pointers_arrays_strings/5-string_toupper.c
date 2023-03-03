13016b395ce5823c0bc39700919cb39e30ddcd33
Nebiyu <nebamami@gmail.com>
2023-03-03 09:51:16 -0800

    to uppercase

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * string_toupper - a function that changes all lowercase
+ * @p:input
+ * Return:char
+ */
+char *string_toupper(char *p)
+{
+	int i;
+
+	i = 0;
+	while (p[i] != '\0')
+	{
+		if (p[i] >= 'a' && p[i] <= 'z')
+		{
+			p[i] =  p[i] - 32;
+		}
+		p[i] =  p[i];
+		i++;
+	}
+}

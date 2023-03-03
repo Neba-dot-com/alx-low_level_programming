698ed16c797b84bd7bccd04011748598b547515c
Nebiyu <nebamami@gmail.com>
2023-03-03 05:04:33 -0800

    compare

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -8,11 +8,16 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-	int compare;
+	int i;
 
-	compare = 0;
-
-	compare = s1[0] - s2[0];
-
-	return (compare);
+	i = 0;
+	while (s1[i] != '\0' && s2[i] != '0')
+	{
+		if ( s1[i] != s2[i])
+		{
+			return (s1[i] - s2[i]);
+		}
+		i++;
+	}
+	return (0);
 }

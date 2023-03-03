934d741385bdfc9c5cc472b47fb6635c5faf1c58
Nebiyu <nebamami@gmail.com>
2023-03-03 08:44:00 -0800

    reverse

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * reverse_array- reverse array
+ * @n:input
+ * @a:input
+ * Return:void
+ */
+void reverse_array(int *a, int n)
+{
+	int i;
+
+	i = n - 1;
+	while (i >= 0)
+	{
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+		printf("%d", a[i]);
+		i--;
+	}
+}

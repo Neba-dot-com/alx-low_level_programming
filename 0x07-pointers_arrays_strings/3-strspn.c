c2684669ff729f0ed44918df839cc08511f6ffcf
Nebiyu <nebamami@gmail.com>
2023-03-06 14:56:15 -0800

    string length

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * _strspn- a function that get the ne length of a prefix substring.
+ * @s: a string
+ * @accept: a prefix substring
+ *Return: length
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+	unsigned int i;
+
+	while (s[i] == accept[i])
+	{
+		i++;
+	}
+	return (i);
+}

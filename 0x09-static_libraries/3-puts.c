d2969bcdc3100087813246b9779eff76d3b0648e
Nebiyu <nebamami@gmail.com>
2023-03-11 14:04:30 -0800

    jhk

--- 0x09-static_libraries/3-puts.c
+++ 0x09-static_libraries/3-puts.c
@@ -1,9 +1,13 @@
 #include "main.h"
+void _puts(char *str);
+#include<stdio.h>
 /**
- * _puts-puts
- * @s:input s
- * Return:void
+ *main - Entry point
+ *
+ * Return: Always 0 (Success)
  */
-void _puts(char *s)
+int main(void)
 {
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
 }
--- 0x09-static_libraries/_putchar.c
+++ 0x09-static_libraries/_putchar.c
@@ -1,9 +1,12 @@
 #include "main.h"
+#include <unistd.h>
 /**
- * _putchar-print putchar
- * @c:input char
- * Return: int
+ * _putchar - writes the character c to stdout
+ * @c: The character to print
+ * Return: On success 1.
+ * On error, -1 is returned, and errno is set appropriately.
  */
 int _putchar(char c)
 {
+	return (write(1, &c, 1));
 }

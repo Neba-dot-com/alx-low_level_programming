1965d2c65e804a485308ee2becf1e0a6495d4002
Nebiyu <nebamami@gmail.com>
2023-03-13 01:02:55 -0700

    my first commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ * main-a program that prints its name, followed by a new line.
+ * @argc: counter of argument
+ * @argv:array of atring
+ * Return: 0(success)
+ */
+int main(int argc, char *argv[])
+{
+	printf("%s", argv[0]);
+}

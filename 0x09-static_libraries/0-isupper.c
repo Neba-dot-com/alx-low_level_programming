c1705a9584519cb31c453c5db86f4ccaf6586826
Nebiyu <nebamami@gmail.com>
2023-03-13 00:37:18 -0700

    my first commit

--- 0x09-static_libraries/0-isupper.c
+++ 0x09-static_libraries/0-isupper.c
@@ -1,9 +1,17 @@
 #include "main.h"
 /**
- * _isupper-a function which print uppercase
- * @c:input c
- * Return:int
+ * _isupper - checks wether a char is upper or or not
+ * @c: char to check
+ * Return: 0 or 1
  */
 int _isupper(int c)
 {
+	if (c >= 'A' && c <= 'Z')
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
 }

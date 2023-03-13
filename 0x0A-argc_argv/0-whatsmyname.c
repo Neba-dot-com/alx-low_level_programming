c58a4f4ce84947831458500d96211ab1dd064422
Nebiyu <nebamami@gmail.com>
2023-03-13 01:20:26 -0700

    m

--- 0x0A-argc_argv/0-whatsmyname.c
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -6,9 +6,10 @@
  * @argv:array of atring
  * Return: 0(success)
  */
-int main(int argc, char* argv[])
+int main(int argc, char *argv[])
 {
 	int i;
+
 	for (i = 0; i < argc; i++)
 	{
 		printf("%s\n", argv[i]);

07c2ed85652e6bfe8a5e67841c1c56c38067069e
Nebiyu <nebamami@gmail.com>
2023-03-14 14:24:36 -0700

    my first commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,33 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * *_strdup -returns a pointer to a newly allocated space in memory,
+ * @str: a string givens as aparameter
+ * Return:apointer to a newly created space in memory
+ */
+char *_strdup(char *str)
+{
+	int i, j;
+	char *str1;
+
+	if (str == NULL)
+	{
+		return (str);
+	}
+
+	i = 0;
+	while (str[i] != '\0')
+	{
+		i++;
+	}
+	str1 = malloc(sizeof(char) * (i + 1));
+
+	j = 0;
+	while (i >= 0)
+	{
+		str1[j] = str[j];
+		j++;
+		i--;
+	}
+	return (str1);
+}
---
+++ 0x0B-malloc_free/2-str_concat.c
@@ -0,0 +1,29 @@
+
+#include "main.h"
+#include <stdlib.h>
+/**
+ * **alloc_grid - returns a pointer to a 2 dimensional *array of integers.
+ * @width: width
+ * @height: height
+ * Return: returns a pointer to a 2 dimensional array of integers.
+ */
+int **alloc_grid(int width, int height)
+{
+	int **array;
+	int i, j;
+
+	if (height <= 0 || width <= 0)
+		return (NULL);
+
+	array = malloc(sizeof(int) * height * width);
+
+	if (!array)
+		return (NULL);
+
+	for (i = 0; i < width; i++)
+	{
+		for (j = 0; j < height; j++)
+			array[i + height + j] = 0;
+	}
+	return (array);
+}
